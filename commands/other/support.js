5b96d30b13818a64d6a27a65e976d898eee76b45
BrightnessTM <khanolympics@gmail.com>
2020-05-19 09:59:17 -0400

    Add support command

---
+++ commands/other/support.js
@@ -0,0 +1,12 @@
+module.exports = {
+    controls: {
+        name: "support",
+        permission: 10,
+        usage: "support",
+        description: "Shows the link to the bot's support server",
+        enabled: true
+    },
+    do: async (message, client) => {
+        return message.channel.send("Need help with the bot? Join https://discord.gg/GGm6YuX for assistance :wink:");
+    }
+};
